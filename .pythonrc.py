8ab8b504ec975bcbdd40b0470ab37148856241df
Baptiste Fontaine <batifon@yahoo.fr>
2013-10-19 14:59:11 +0200

    Handling of 'b' in Python interpreter resolved. see http://superuser.com/a/373631/95929

--- .pythonrc.py
+++ .pythonrc.py
@@ -9,7 +9,7 @@ except ImportError:
     print("Module readline not available.")
 else:
     import rlcompleter
-    if(sys.platform == 'darwin'):
+    if 'libedit' in readline.__doc__:
         readline.parse_and_bind ("bind ^I rl_complete")
     else:
         readline.parse_and_bind("tab: complete")
