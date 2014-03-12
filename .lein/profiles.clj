7ddfb50cbef49b25af80e5ce080ccc406c0beca6
Baptiste Fontaine <batifon@yahoo.fr>
2014-03-12 22:31:08 +0100

    lein profiles

---
+++ .lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user {:plugins [[lein-cloverage                    "1.0.2"]
+                  [lein-fore-prob                    "0.1.2"]
+                  [lein-ancient                      "0.5.4"]
+                  [jonase/eastwood                   "0.1.0"]
+                  [lein-drip                         "0.1.1-SNAPSHOT"]
+                  [com.jakemccrary/lein-test-refresh "0.3.4"]
+                  [lein-pprint                       "1.1.1"]]
+        :signing {:gpg-key "E5B26621"}}}
