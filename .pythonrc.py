bf8aef30db2661b7f1833433822a346da27e4b5b
Baptiste Fontaine <batifon@yahoo.fr>
2014-03-12 22:33:57 +0100

    no need for a colored python prompt

--- .pythonrc.py
+++ .pythonrc.py
@@ -38,56 +38,16 @@ finally:
     del rlcompleter
 
 WELCOME=''
-# Color Support
-class TermColors(dict):
-    """Gives easy access to ANSI color codes. Attempts to fall back to no color
-    for certain TERM values. (Mostly stolen from IPython.)"""
-
-    COLOR_TEMPLATES = (
-        ("Black"       , "0;30"),
-        ("Red"         , "0;31"),
-        ("Green"       , "0;32"),
-        ("Brown"       , "0;33"),
-        ("Blue"        , "0;34"),
-        ("Purple"      , "0;35"),
-        ("Cyan"        , "0;36"),
-        ("LightGray"   , "0;37"),
-        ("DarkGray"    , "1;30"), ("LightRed"    , "1;31"),
-        ("LightGreen"  , "1;32"),
-        ("Yellow"      , "1;33"),
-        ("LightBlue"   , "1;34"),
-        ("LightPurple" , "1;35"),
-        ("LightCyan"   , "1;36"),
-        ("White"       , "1;37"),
-        ("Normal"      , "0"),
-    )
-
-    NoColor = ''
-    _base  = '\001\033[%sm\002'
-
-    def __init__(self):
-        if os.environ.get('TERM') in ('xterm-color', 'xterm-256color', 'linux',
-                                    'screen', 'screen-256color', 'screen-bce'):
-            self.update(dict([(k, self._base % v) for k,v in self.COLOR_TEMPLATES]))
-        else:
-            self.update(dict([(k, self.NoColor) for k,v in self.COLOR_TEMPLATES]))
-_c = TermColors()
-
-
-
-import sys
-# Enable Color Prompts
-sys.ps1 = '%s>>> %s' % (_c['Green'], _c['Normal'])
-sys.ps2 = '%s... %s' % (_c['Red'], _c['Normal'])
 
 # Enable Pretty Printing for stdout
 def my_displayhook(value):
     if value is not None:
         try:
             import __builtin__
-            __builtin__._ = value
         except ImportError:
-            __builtins__._ = value
+            pass
+
+        __builtins__._ = value
 
         import pprint
         pprint.pprint(value)
@@ -95,6 +55,7 @@ def my_displayhook(value):
 
 sys.displayhook = my_displayhook
 
+# Django
 if 'DJANGO_SETTINGS_MODULE' in os.environ:
     from django.db.models.loading import get_models
     from django.test.client import Client
