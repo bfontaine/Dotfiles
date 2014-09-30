b1e3ce462584e3123e1ac4aac5b5016253d5c582
Baptiste Fontaine <batifon@yahoo.fr>
2014-09-30 09:06:14 +0200

    [vim] Coq support

--- vim.sh
+++ vim.sh
@@ -171,6 +171,8 @@ vim_script syntax/json          10853 # JSON
 
 gh_bundle guns/vim-clojure-static  # Clojure
 gh_bundle kchmck/vim-coffee-script # CoffeeScript
+gh_bundle def-lkb/vimbufsync       # Coq
+gh_bundle the-lambda-church/coquille
 gh_bundle rhysd/vim-crystal        # Crystal
 gh_bundle hailu/vim-css3-syntax    # CSS3
 gh_bundle dag/vim-fish             # Fish
