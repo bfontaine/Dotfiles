d8e0bed08c5e9bce4baf78fc9f3025fc936c67ff
Baptiste Fontaine <b@ptistefontaine.fr>
2016-05-11 10:01:04 +0200

    [vim] Add Clojure tools

--- vim.sh
+++ vim.sh
@@ -107,6 +107,12 @@ gh_bundle tomtom/tlib_vim
 gh_bundle MarcWeber/vim-addon-mw-utils
 gh_bundle garbas/vim-snipmate
 
+# Vim-fireplace and its dependencies (Clojure toolset)
+gh_bundle tpope/vim-salve         # Static support for Leiningen
+gh_bundle tpope/vim-projectionist
+gh_bundle tpope/vim-dispatch      # Async build & test dispatcher
+gh_bundle tpope/vim-fireplace     # Clojure REPL support
+
 gh_bundle tell-k/vim-autopep8    # autopep8
 gh_bundle ConradIrwin/vim-bracketed-paste # Bracketed paste
 gh_bundle Raimondi/delimitMate   # DelimitMate
