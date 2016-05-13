547c44b65e82dcbebdbdda9d827baf173ea9a9e2
Baptiste Fontaine <b@ptistefontaine.fr>
2016-05-13 15:13:05 +0200

    [lein] Update Eastwood

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,7 +1,7 @@
 {:user {:plugins [[lein-cloverage                    "1.0.2"]
                   [lein-fore-prob                    "0.1.2"]
                   [lein-ancient                      "0.5.4"]
-                  [jonase/eastwood                   "0.1.0"]
+                  [jonase/eastwood                   "0.2.1"]
                   [lein-drip                         "0.1.1-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.3.4"]
                   [lein-pprint                       "1.1.1"]]
