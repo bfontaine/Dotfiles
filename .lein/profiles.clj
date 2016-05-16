9773319763c71ab87f6f4eb81bf543cf1c6f3d5c
Baptiste Fontaine <b@ptistefontaine.fr>
2016-05-16 10:23:20 +0200

    [lein] Add cider

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,9 @@
 {:user {:plugins [[lein-cloverage                    "1.0.2"]
                   [lein-fore-prob                    "0.1.2"]
                   [lein-ancient                      "0.5.4"]
-                  [jonase/eastwood                   "0.2.1"]
+                  [jonase/eastwood                   "0.2.3"]
                   [lein-drip                         "0.1.1-SNAPSHOT"]
                   [com.jakemccrary/lein-test-refresh "0.3.4"]
-                  [lein-pprint                       "1.1.1"]]
+                  [lein-pprint                       "1.1.1"]
+                  [cider/cider-nrepl                 "0.12.0"]]
         :signing {:gpg-key "E5B26621"}}}
