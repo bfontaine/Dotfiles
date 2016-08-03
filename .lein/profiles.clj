c876b07dce9beae16e780278389a4b318f5926e5
Baptiste Fontaine <b@ptistefontaine.fr>
2016-08-03 18:20:30 +0200

    [lein] Bump lein-ancient

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[lein-cloverage                    "1.0.2"]
                   [lein-fore-prob                    "0.1.2"]
-                  [lein-ancient                      "0.5.4"]
+                  [lein-ancient                      "0.6.10"]
                   [jonase/eastwood                   "0.2.3"]
                   [com.jakemccrary/lein-test-refresh "0.3.4"]
                   [lein-pprint                       "1.1.1"]
