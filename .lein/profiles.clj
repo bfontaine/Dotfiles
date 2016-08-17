df9cbd86894d566123f1949107de25a7681f47fd
Baptiste Fontaine <b@ptistefontaine.fr>
2016-08-17 10:48:33 +0200

    [lein] Add lein-kibit

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,6 +4,7 @@
                   [jonase/eastwood                   "0.2.3"]
                   [com.jakemccrary/lein-test-refresh "0.3.4"]
                   [lein-pprint                       "1.1.1"]
+                  [lein-kibit                        "0.1.2"]
                   [cider/cider-nrepl                 "0.12.0"]
                   [venantius/ultra                   "0.4.1"]]
         :signing {:gpg-key "E5B26621"}}}
