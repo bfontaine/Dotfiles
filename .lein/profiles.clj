9cf52e0e7c7434c3fa324978c0aab76aa625727f
Baptiste Fontaine <b@ptistefontaine.fr>
2016-08-30 12:23:21 +0200

    [lein] Remove unused plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,10 +1,7 @@
 {:user {:plugins [[lein-cloverage                    "1.0.6"]
                   [lein-fore-prob                    "0.1.2"]
                   [lein-ancient                      "0.6.10"]
-                  [jonase/eastwood                   "0.2.3"]
-                  [com.jakemccrary/lein-test-refresh "0.3.4"]
-                  [lein-pprint                       "1.1.1"]
-                  [lein-kibit                        "0.1.2"]
-                  [cider/cider-nrepl                 "0.12.0"]
+                  [lein-pprint                       "1.1.2"]
+                  [cider/cider-nrepl                 "0.13.0"]
                   [venantius/ultra                   "0.4.1"]]
         :signing {:gpg-key "E5B26621"}}}
