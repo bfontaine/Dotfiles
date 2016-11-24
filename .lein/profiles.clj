5a3b4a9068f9edb80095ff0f7a9485982f2f8e9e
Baptiste Fontaine <b@ptistefontaine.fr>
2016-11-24 10:03:49 +0100

    [clojure] bump plugins

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -3,6 +3,6 @@
                   [lein-ancient                      "0.6.10"]
                   [lein-pprint                       "1.1.2"]
                   [lein-figwheel                     "0.5.4-7"]
-                  [cider/cider-nrepl                 "0.13.0"]
+                  [cider/cider-nrepl                 "0.14.0"]
                   [venantius/ultra                   "0.5.0"]]
         :signing {:gpg-key "E5B26621"}}}
