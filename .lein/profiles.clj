965c437e0f8640d3482ada150765f97dcba10081
Baptiste Fontaine <b@ptistefontaine.fr>
2017-06-28 11:09:54 +0200

    [lein] inject 'refresh & 'doc

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -17,7 +17,23 @@
 
         :dependencies [; Code quality
                        [slamhound                    "1.5.5"]
-                       [walmartlabs/datascope        "0.1.1"]]
+                       [walmartlabs/datascope        "0.1.1"]
+
+                       [org.clojure/tools.namespace  "0.2.11"]
+
+                       ;; Inject symbols in the REPL's global ns
+                       ;; http://dev.solita.fi/2014/03/18/pimp-my-repl.html
+                       ;; http://docs.caudate.me/lucidity/lucid-core.html#core-inject
+                       [im.chit/lucid.core.inject    "1.3.9"]]
 
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+
+        :injections [(require 'lucid.core.inject)
+                     (lucid.core.inject/in
+                       ;; help function
+                       [clojure.repl :refer [doc]]
+
+                       ;; Refresh namespaces without reloading the REPL
+                       [clojure.tools.namespace.repl :refer [refresh]])]
+
         :signing {:gpg-key "E5B26621"}}}
