7a41f747ecf0c771859042d467ab7db6c90df0dc
Baptiste Fontaine <b@ptistefontaine.fr>
2018-08-16 11:30:29 +0200

    [lein] remove injections
    they don't work anyway

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -15,23 +15,10 @@
                   [lein-figwheel                     "0.5.10"]
                   [venantius/ultra                   "0.5.1"]]
 
-        :dependencies [[org.clojure/tools.namespace  "0.2.11"]
-
-                       ;; Inject symbols in the REPL's global ns
-                       ;; http://dev.solita.fi/2014/03/18/pimp-my-repl.html
-                       ;; http://docs.caudate.me/lucidity/lucid-core.html#core-inject
-                       [im.chit/lucid.core.inject    "1.3.9"]]
+        :dependencies [[org.clojure/tools.namespace  "0.2.11"]]
 
         :ultra {:stacktraces false ; don't break my stacktraces thanks
                 :repl {:sort-keys false ; don't sort collections before printing
                        }}
-
-        :injections [(require 'lucid.core.inject)
-                     (lucid.core.inject/in
-                       ;; help function
-                       [clojure.repl :refer [doc]]
-
-                       ;; Refresh namespaces without reloading the REPL
-                       [clojure.tools.namespace.repl :refer [refresh]])]
 
         :signing {:gpg-key "E5B26621"}}}
