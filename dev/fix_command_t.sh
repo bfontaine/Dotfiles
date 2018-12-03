506b2281d2a0ef400eb1035fb87871e7121d10c6
Baptiste Fontaine <b@ptistefontaine.fr>
2018-12-03 17:12:23 +0100

    fix fix_command_t.sh

--- dev/fix_command_t.sh
+++ dev/fix_command_t.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-cd ~/.vim/bundle/Command-T/ruby/command-t/ || exit
+cd ~/.vim/bundle/Command-T/ruby/command-t/ext/command-t || exit
 [ -f Makefile ] && make clean
 ruby extconf.rb
 make
