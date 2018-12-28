fa3943858181ca20315d2ec4e413c17b91d19f47
Baptiste Fontaine <b@ptistefontaine.fr>
2018-12-28 11:26:20 +0100

    fix ruby in dev/fix_command_t.sh

--- dev/fix_command_t.sh
+++ dev/fix_command_t.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 cd ~/.vim/bundle/Command-T/ruby/command-t/ext/command-t || exit
+
+ruby=$(vim --version | grep -Eo -m 1 '/\S+ruby/[0-9.]+/lib' | head -1 | sed 's%lib$%bin/ruby%')
+
+if [ -z "$ruby" ] || [ ! -x "$ruby" ]; then
+  ruby="ruby"
+else
+  echo "Using ruby from $ruby"
+fi
+
 [ -f Makefile ] && make clean
-ruby extconf.rb
+$ruby extconf.rb
 make
