8a1b63309f0cb36567e524284739d9cdd518287d
Baptiste Fontaine <b@ptistefontaine.fr>
2020-11-20 10:25:22 +0100

    Add dev/linux_pro_setup.sh

---
+++ dev/linux_pro_setup.sh
@@ -0,0 +1,28 @@
+#! /bin/bash -ex
+sudo apt update
+sudo apt upgrade
+sudo apt install vim git
+
+mkdir -p ~/GitHub
+pushd ~/GitHub
+git clone http://github.com/bfontaine/Dotfiles.git
+pushd Dotfiles
+for f in .ackrc .bash_profile .bashrc .bashrc_linux .ctags .cvsignore .emacs \
+  .gemrc .gitattributes .gitconfig .hushlogin .inputrc .irbrc .pythonrc.py \
+  .safe-rm .up-commands .vimrc .zshrc \
+; do
+  rm "$HOME/$f"
+  ln -s "$f" "$HOME/$f"
+done
+
+cp -r .bash_utils ~/
+mkdir -p ~/.config
+cp -r .config/fish ~/.config/
+
+mkdir -p ~/bin
+cp bin/* ~/bin/
+
+# Vim
+cp -r .vim ~/
+./setup.sh
+
