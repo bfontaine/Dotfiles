9b5d193795a94bad6296889c8da534251de9406d
Baptiste Fontaine <baptiste@bixoto.com>
2022-12-19 19:16:11 +0000

    lein: bump deps

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,33 +1,19 @@
-{:user {:plugins [; https://github.com/clojure-emacs/cider-nrepl#via-leiningen
-                  [cider/cider-nrepl                 "0.21.1"]
-                  [refactor-nrepl                    "2.4.0"]
-
-                  [lein-cloverage                    "1.0.13"]
-                  [lein-fore-prob                    "0.1.2"]
-
-                  [lein-nvd "1.1.1"]
-
-                  ; Check for outdated dependencies
-                  [lein-ancient                      "0.6.15"]
-
-                  ; Check for bad coding style
-                  [lein-bikeshed                      "0.5.1"]
-                  [lein-kibit                         "0.1.6"]
-                  [jonase/eastwood                    "0.3.4"]
-
-                  ;; lein-test when something changes
-                  [com.jakemccrary/lein-test-refresh "0.23.0"]
-
-                  [lein-figwheel                     "0.5.18"]
-                  [venantius/ultra                   "0.6.0"]]
-
+{:user {:dependencies [[org.clojure/tools.namespace "1.3.0"]],
         :middleware [cider-nrepl.plugin/middleware
-                     refactor-nrepl.plugin/middleware]
-
-        :dependencies [[org.clojure/tools.namespace  "0.2.11"]]
-
-        :ultra {:stacktraces false ; don't break my stacktraces thanks
+                     refactor-nrepl.plugin/middleware],
+        :plugins [; https://github.com/clojure-emacs/cider-nrepl#via-leiningen
+                  [cider/cider-nrepl "0.29.0"] [refactor-nrepl "3.6.0"]
+                  [lein-cloverage "1.2.4"] #_[lein-fore-prob "0.1.2"]
+                  [lein-nvd "2.0.0"]
+                  ; Check for outdated dependencies
+                  [lein-ancient "0.7.0"]
+                  ; Check for bad coding style
+                  [lein-bikeshed "0.5.2"] [lein-kibit "0.1.8"]
+                  [jonase/eastwood "1.3.0"]
+                  ;; lein-test when something changes
+                  [com.jakemccrary/lein-test-refresh "0.25.0"]
+                  [lein-figwheel "0.5.20"] #_[venantius/ultra "0.6.0"]],
+        :signing {:gpg-key "E5B26621"},
+        :ultra {:stacktraces false, ; don't break my stacktraces thanks
                 :repl {:sort-keys false ; don't sort collections before printing
-                       }}
-
-        :signing {:gpg-key "E5B26621"}}}
+                      }}}}
