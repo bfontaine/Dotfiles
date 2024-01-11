9c8515ff35ec0d7dae5f31da7b0e5cb3d7703d20
Baptiste Fontaine <baptiste@bixoto.com>
2024-01-11 15:51:02 +0000

    python: fix pp

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,7 +1,7 @@
 def pp(x):
     import json
     import sys
-    json.dump(x, sys.stdout, sort_keys=True, indent=2)
+    json.dump(x, sys.stdout, sort_keys=True, ensure_ascii=False, indent=2)
 
 
 try:
