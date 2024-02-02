4327c0a8747646ead539a3e2556ff23019ac5e6a
Baptiste Fontaine <baptiste@bixoto.com>
2024-02-02 17:58:35 +0000

    [python] don't override readline history length

--- .pythonrc.py
+++ .pythonrc.py
@@ -3,10 +3,3 @@ def pp(x):
     import sys
     json.dump(x, sys.stdout, sort_keys=True, ensure_ascii=False, indent=2)
 
-
-try:
-    import readline
-except ImportError:
-    pass
-else:
-    readline.set_history_length(10000)
